679fdeeea649fd5f8e8a005fd5e1d8eda2ff742c
Jude Sajith <judesajith.aj@gmail.com>
2023-04-24 23:51:23 +0530

    ssss

--- app.py
+++ app.py
@@ -163,14 +163,62 @@ try:
         upload_col.write(st.session_state.table_of_contents)
 
         # upload_col.success("TOC loaded, Go to the next tab")
+    column1, column2 = st.columns(2)
+
+    data = st.session_state.table_of_contents
+
+    topic_data = {list(t.keys())[0]: list(t.values())[0] for t in data["Topics"]}
+    if "topic_data" not in st.session_state:
+        st.session_state['topic_data'] = topic_data
+    column1.title("Topics and Subtopics Editor")
+
+    topic_name = column1.text_input("Enter topic name:")
+
+    if column1.button("Save Topic"):
+        if topic_name not in st.session_state['topic_data']:
+            st.session_state['topic_data'][topic_name] = []
+            update_json(topic_data)
+
+    topic_options = list(st.session_state['topic_data'].keys())
+    selected_topic = column1.selectbox("Select a topic:", topic_options)
+
+    subtopics = st.session_state['topic_data'][selected_topic]
+
+    column1.write("## Subtopics:")
+    subtopics_input = column1.multiselect("", subtopics, default=subtopics)
+
+    if column1.button("Save Subtopics"):
+        st.session_state['topic_data'][selected_topic] = subtopics_input
+        update_json(st.session_state['topic_data'])
+    add = column1.button("Add Subtopic")
+    if "add" in st.session_state  or add:
+        st.session_state['add'] = True
+        new_subtopic = column1.text_input("Enter subtopic name:")
+        if column1.button("Update"):
+            if new_subtopic not in st.session_state['topic_data'][selected_topic]:
+                st.session_state['topic_data'][selected_topic].append(new_subtopic)
+                #column1.write(st.session_state['topic_data'][selected_topic])
+                #update_json(st.session_state['topic_data'])
+                add= None
+                st.session_state['add'] = False
+                st.experimental_rerun()
+
+    column2.write("## Updated JSON:")
+    # column2.json(st.session_state['topic_data'])
+
+    for topic, subtopics in st.session_state['topic_data'].items():
+        column2.markdown(f"**{topic}**")
+        for subtopic in subtopics:
+            column2.write(f"- {subtopic}")
+
     
-    
+
 
     quer = extract_col.button("Extract Selected")
 
 
     new_dict = {}
-    for topic in st.session_state.table_of_contents["Topics"]:
+    for topic in st.session_state['topic_data']["Topics"]:
         for key, value in topic.items():
             # Add a description for the topic
             new_dict[key] = {'content': '', 'Subtopics': []}
