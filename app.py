e096d663f467903b8f97b91c0ca724581e4c4161
Jude Sajith <judesajith.aj@gmail.com>
2023-04-25 03:26:21 +0530

    ssss

--- app.py
+++ app.py
@@ -297,7 +297,7 @@ try:
         extract_col.markdown(f"**{topic_key}**")
         extract_col.write(topic_value["content"])
     
-        expander = st.expander("Subtopics")
+        expander = st.expander(f"{topic_key}")
         for subtopic in topic_value["Subtopics"]:
             expander.markdown(f"**{subtopic['Subtopic']}**")
             expander.write(subtopic["content"])
