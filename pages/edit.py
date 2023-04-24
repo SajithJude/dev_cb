d0d2aa4ed771e9c6f7cea44d6f782a736500a04b
Jude Sajith <judesajith.aj@gmail.com>
2023-04-24 17:45:06 +0530

    ssss

--- pages/edit.py
+++ pages/edit.py
@@ -4,8 +4,6 @@ import json
 def update_json(topic_data):
     with open("output.json", "w") as f:
         json.dump({"Topics": [{k: v} for k, v in topic_data.items()]}, f)
-    col2.write("## Updated JSON:")
-    col2.json({"Topics": [{k: v} for k, v in topic_data.items()]})
 
 json_data = '''
 {
@@ -40,32 +38,37 @@ json_data = '''
 col1, col2 = st.columns(2)
 data = json.loads(json_data)
 topic_data = {list(t.keys())[0]: list(t.values())[0] for t in data["Topics"]}
-
+if "topic_data" not in st.session_state:
+      st.session_state['topic_data'] = topic_data
 col1.title("Topics and Subtopics Editor")
 
-topic_name = col1.text_input("Enter New topic name to add:")
+topic_name = col1.text_input("Enter topic name:")
 
-if col1.button("Add & New Topic") and topic_name and topic_name not in topic_data:
-    topic_data[topic_name] = []
-    update_json(topic_data)
+if col1.button("Save Topic"):
+    if topic_name not in st.session_state['topic_data']:
+        st.session_state['topic_data'][topic_name] = []
+        update_json(topic_data)
 
-if "selected_topic" not in st.session_state:
-    st.session_state.selected_topic = list(topic_data.keys())[0]
+topic_options = list(st.session_state['topic_data'].keys())
+selected_topic = col1.selectbox("Select a topic:", topic_options)
 
-topic_options = list(topic_data.keys())
-st.session_state.selected_topic = col1.selectbox("Select a topic:", topic_options, index=topic_options.index(st.session_state.selected_topic))
-
-subtopics = topic_data[st.session_state.selected_topic]
+subtopics = st.session_state['topic_data'][selected_topic]
 
 col1.write("## Subtopics:")
 subtopics_input = col1.multiselect("", subtopics, default=subtopics)
 
 if col1.button("Save Subtopics"):
-    topic_data[st.session_state.selected_topic] = subtopics_input
-    update_json(topic_data)
+    st.session_state['topic_data'][selected_topic] = subtopics_input
+    update_json(st.session_state['topic_data'])
 
 if col1.button("Add Subtopic"):
     new_subtopic = col1.text_input("Enter subtopic name:")
-    if new_subtopic:
-        topic_data[st.session_state.selected_topic].append(new_subtopic)
-        update_json(topic_data)
+    if col1.button("Update"):
+        if new_subtopic not in st.session_state['topic_data']:
+            st.session_state['topic_data'][selected_topic].append(new_subtopic)
+            st.write(st.session_state['topic_data'][selected_topic])
+            #update_json(st.session_state['topic_data'])
+            #st.experimental_rerun()
+
+col2.write("## Updated JSON:")
+col2.json({"Topics": [{k: v} for k, v in st.session_state['topic_data'].items()]})
