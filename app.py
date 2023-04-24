b59c906ed04a62dc38e12d3cbdebdd909ae61a8d
Jude Sajith <judesajith.aj@gmail.com>
2023-04-25 03:14:09 +0530

    sssss

--- app.py
+++ app.py
@@ -291,7 +291,7 @@ try:
 
             updated_json = json.dumps(st.session_state.new_dict, indent=2)
         
-    # extract_col.write(st.session_state.new_dict)
+    extract_col.write(st.session_state.new_dict)
     for topic_key, topic_value in st.session_state.new_dict.items():
         extract_col.markdown(f"**{topic_key}**")
         extract_col.write(topic_value["content"])
