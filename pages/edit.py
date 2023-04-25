75f9119faf80515ce35faab27b1258e08973ccb1
Jude Sajith <judesajith.aj@gmail.com>
2023-04-25 11:11:10 +0530

    ssss

--- pages/edit.py
+++ pages/edit.py
@@ -21,7 +21,7 @@ if uploaded_file is not None:
     with fitz.open(uploaded_file.name) as doc:
         for page_index in range(len(doc)):
             page = doc[page_index]
-            image_list = page.getImageList()
+            image_list = page.get_page_images(page_index,full=True)
             if image_list:
                 st.write(f"[+] Found a total of {len(image_list)} images in page {page_index}")
             else:
