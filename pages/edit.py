80ac9519b2681920d030bbb8520c26b17efc548e
Jude Sajith <judesajith.aj@gmail.com>
2023-04-25 14:09:03 +0530

    ssss

--- pages/edit.py
+++ pages/edit.py
@@ -155,12 +155,12 @@ if "add" in st.session_state  or add:
 image_files = [f for f in os.listdir("images") if f.endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))]
 
 selected_images = []
-for image in image_files:
-    cols = cycle(st.columns(4))
-    for idx, image in enumerate(image_files):
-        next(cols).image(os.path.join("images", image), width=150)
-        checkbox = colu1.checkbox(f"select{image}", key=str(image))
-        colu2.image(os.path.join("images", image), use_column_width=True)
+# for image in image_files:
+cols = cycle(st.columns(4))
+for idx, image in enumerate(image_files):
+    next(cols).image(os.path.join("images", image), width=150)
+    checkbox = colu1.checkbox(f"select{image}", key=str(image))
+    colu2.image(os.path.join("images", image), use_column_width=True)
 
 
 
