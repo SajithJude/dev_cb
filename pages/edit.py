050e33c71a12da81128730b5b6f6f4e243934979
Jude Sajith <judesajith.aj@gmail.com>
2023-04-25 11:05:29 +0530

    ssssss

--- pages/edit.py
+++ pages/edit.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 
 
+
 import streamlit as st
 
 # upload file
@@ -17,7 +18,22 @@ if uploaded_file is not None:
         f.write(uploaded_file.getbuffer())
 
     # display PDF file
-    import fitz
     with fitz.open(uploaded_file.name) as doc:
-        for page in doc:
-            st.image(page.get_pixmap())
+        for page_index in range(len(pdf_file)):
+            page = pdf_file[page_index]
+            image_list = page.getImageList()
+            if image_list:
+                st.write(f"[+] Found a total of {len(image_list)} images in page {page_index}")
+            else:
+                st.write("[!] No images found on page", page_index)
+            for image_index, img in enumerate(page.getImageList(), start=1):
+                
+                # get the XREF of the image
+                xref = img[0]
+                # extract the image bytes
+                base_image = pdf_file.extractImage(xref)
+                image_bytes = base_image["image"]
+                # get the image extension
+                image_ext = base_image["ext"]
+                # load it to PIL
+                image = Image.open(io.BytesIO(image_bytes))
