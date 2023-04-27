986869cb496b4aa8402de268c2c3e32e6472b047
Jude Sajith <judesajith.aj@gmail.com>
2023-04-27 14:45:54 +0530

    ssss

--- app.py
+++ app.py
@@ -480,14 +480,14 @@ try:
             subtopic_dict['content'] = edit_col.text_area(f"Subtopic {subtopic_name} under topic {topic} :", value=content)
     # pass 
 
-    save = edit_col.button("Save")
-    if save:
+    # save = edit_col.button("Save")
+    # if save:
         # with open("saveedit.json", "w") as f:
         #     json.dump(st.session_state.new_dict, f,indent=2)
-        with open("newdict.json", "r") as f:
-            savedit = json.load(f)
-            st.session_state.new_dict = savedit
-            edit_col.write(st.session_state.new_dict)
+    with open("newdict.json", "r") as f:
+        savedit = json.load(f)
+        st.session_state.new_dict = savedit
+        edit_col.write(st.session_state.new_dict)
 
 
 
