5dd0e83e9e1a687baea1576e030d4afdb2940ee6
Thanoraj <e18352@eng.pdn.ac.lk>
2023-04-27 16:02:12 +0530

    gfreg

--- app.py
+++ app.py
@@ -413,7 +413,7 @@ try:
     
     new_query = bmiscol.text_input("Name of the missing Subtopic")
     topic_belong = bmiscol.selectbox("Select the belonging topic",topic_names)
-    query_again = bmiscol.checkbox("extract missing")
+    query_again = bmiscol.button("extract missing")
     
     if query_again:
         
