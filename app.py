3883a938fcae37b837f5c697ee9507709e01d37d
Jude Sajith <judesajith.aj@gmail.com>
2023-04-27 08:44:50 +0530

    ssss

--- app.py
+++ app.py
@@ -382,15 +382,16 @@ try:
     with open("newdict.json", "r") as f:
         st.session_state.new_dict = json.load(f)    
 
-    if 'extracted_data' not in st.session_state:
-        st.session_state.extracted_data = []
+    # if 'extracted_data' not in st.session_state:
+    #     st.session_state.extracted_data = []
     
     pages_files = [f for f in os.listdir("pages") if f.endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))]
     with open("newdict.json", "r") as f:
         st.session_state.new_dict = json.load(f)
     if pages_files:
         selected_page = pagecol.number_input("Change page number to compare:",step=1)
-        
+        with open("newdict.json", "r") as f:
+            st.session_state.new_dict = json.load(f)
         selected_image = f"page-{selected_page}.png"
         # Display the selected image
         if selected_image:
