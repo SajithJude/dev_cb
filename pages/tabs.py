4375d55898fa8e5c7847cbd63f1485bd48f1b11b
Jude Sajith <judesajith.aj@gmail.com>
2023-04-27 08:13:54 +0530

    ssss

--- pages/tabs.py
+++ pages/tabs.py
@@ -10,13 +10,13 @@ url = "https://coursebot2.flipick.com/couresbuilderapi/api/Course/ImportCourseZi
 filesinsidefolder = st.selectbox("Select a zip file", [f for f in os.listdir("images") if f.endswith(('.zip'))])
 
 
-if st.button("send")
-payload = {}
-files=[
-  ('ImportZip',('Chapter5.zip',open(os.path.join("images", filesinsidefolder), 'rb'),'application/zip'))
-]
-headers = {}
+if st.button("send"):
+    payload = {}
+    files=[
+    ('ImportZip',('Chapter5.zip',open(os.path.join("images", filesinsidefolder), 'rb'),'application/zip'))
+    ]
+    headers = {}
 
-response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
 
-st.write(response.text)
+    st.write(response.text)
