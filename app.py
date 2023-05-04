f6d60a565ee0bbcd18870e4ee65740a600d36755
Jude Sajith <judesajith.aj@gmail.com>
2023-05-04 14:42:58 +0530

    ssss

--- app.py
+++ app.py
@@ -531,8 +531,8 @@ try:
             expander.write(subtopic["content"])
                     
         
-     with open(f'{course_name}.json', 'w') as outfile:
-
+    with open(f'{course_name}.json', 'w') as outfile:
+        
         json.dump({
                     'course_name': course_name,
                     'data': st.session_state.new_dict
