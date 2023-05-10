1a3efcbc063951707803108b5cf64946237be551
Jude Sajith <judesajith.aj@gmail.com>
2023-05-10 06:53:11 +0530

    ssss

--- app.py
+++ app.py
@@ -385,7 +385,7 @@ try:
         upload_col.success("TOC loaded, Go to the next tab")
 
 except json.JSONDecodeError as e:
-    table_of_contents = json.loads(str_to.strip())
+    table_of_contents = json.loads(str(str_to))
     st.session_state.table_of_contents = table_of_contents
     upload_col.write(st.session_state.table_of_contents)
     # upload_col.error("Invalid JSON format. Please check your input.")
